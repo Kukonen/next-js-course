db93fabdfb4da738b41da03998dffc99fcb4180f
Evgeniy <ekukonen@gmail.com>
2021-09-08 18:52:07 +0300

    fix footer grid

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,11 +1,13 @@
 import { IFooterProps } from "./Footer.props";
 import styles from './Footer.module.css';
+import {format} from 'date-fns';
+import cn from 'classnames';
 
 export const Footer = ({...props}:IFooterProps) : JSX.Element => {
     return (
         <div {...props} className={styles.footer}>
             <span className={styles.copyright}>
-                OwlTop © 2020 - 2021 Все права защищены
+                OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены
             </span>
             <a href="#" className={styles.link}>
                 Пользовательское соглашение
